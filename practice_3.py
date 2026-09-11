fdbb233f124b56cac5f974b388194496871a0e6d
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-11 15:51:04 +0500

    Create practice_3.py

---
+++ practice_3.py
@@ -0,0 +1,21 @@
+
+'''Problem 4 — Rectangle
+
+Ask the user for:
+
+length
+width
+
+Calculate:
+
+Area = length × width
+Perimeter = 2 × (length + width)
+
+Print both results.'''
+
+user1 = int(input("Enter length: "))
+user2 =  int(input("Enter width: "))
+
+# area = lenght * width
+
+print(f"The total area is {user1 * user2} ")
