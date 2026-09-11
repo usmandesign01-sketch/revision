6baa6d90342087298f467196c74caaebf022baf9
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-11 15:40:02 +0500

    Create practice_2_question.py

---
+++ practice_2_question.py
@@ -0,0 +1,10 @@
+'''Problem 3 — Age After 5 Years
+
+Ask the user's current age.
+
+Calculate and print:
+
+Your age after 5 years will be: 23'''
+
+user = int(input("Enter your current age: "))
+print(f"You age after 5 year will be {user + 5}")
