5999edaae032435eabd93d3a9c781342446b2296
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-22 22:54:17 +0500

    Update 02.py

--- 🐍 DAY 6 — Mixed Python Revision/02.py
+++ 🐍 DAY 6 — Mixed Python Revision/02.py
@@ -2,5 +2,19 @@
 
 
 
+# marks = [443, 520, 1008, 399, 443, 801]
+# print("avg:", sum(marks) / len(marks))
+
+
 marks = [443, 520, 1008, 399, 443, 801]
-print("avg:", sum(marks) / len(marks))
+standard_average = sum(marks) / len(marks)
+print("standard average:", standard_average)
+
+highest_marks = max(marks)
+print("highest:", highest_marks)
+
+# what is scaled average?
+# scaled average is the average of the marks after scaling them to a certain range, usually to make them comparable or to fit within a specific grading system.
+
+scaled_avg_out_of_100 = (standard_average / highest_marks) * 100
+print("scaled average out of 100:", scaled_avg_out_of_100)
