4535246a237805d64ddffa2a034b2562bbb12b19
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-24 15:54:55 +0500

    Update counter_accumulator_practice.py

--- 🐍 DAY 6 — Mixed Python Revision/counter_accumulator_practice.py
+++ 🐍 DAY 6 — Mixed Python Revision/counter_accumulator_practice.py
@@ -1,3 +1,74 @@
+# Counter:
+# A counter is simply a variable used to keep track of how many times somethings happens.
+
+# Basic pattern:
+# count = 0
+
+# count +=1
+# count +=1
+# count +=1
+
+# print(count) # output 3
+
+'''
+Counter inside a Loop
+
+'''
+# count = 0
+# for i in range(5):
+#     count += 1
+# print(count)
+
+'''
+Accumulator
+An accumulator is a variable that gradually build the total/result
+
+Basic example:
+total = 0
+
+total += 10
+total += 20
+total += 30
+
+print(total) # Output 60
+'''
+# num = [10,23,45,67,90,-23]
+# total = 0
+# for x in num:
+#     total = total + x
+# print(total)
+
+# Accumulator answers: HOW MUCH?/ WHAT IS THE TOTAL?
+
+# EXAMPLES:
+# Total salary
+# Total money
+# Total price
+
+# SUMMARY: COUNTER COUNT THINGS AND ACCUMULATOR COUNT TOTAL
+
+print("====This is EXAMPLE of accumulator and counter====")
+user_entered = []
+total = 0
+total_even = 0
+for i in range(6):
+    user = int(input(f"Enter a number {i + 1}: "))
+    user_entered.append(user)
+print()
+print(f"You enter these number {user_entered}")
+
+for x in user_entered:
+    total = total + x
+
+    if x % 2 == 0:
+        total_even += 1
+
+print("\n----- Result -----")
+print(f"Total: {total}")
+print(f"Total_odd: {total_even}")
+
+
+# **************************************************************************************************************
 # Create an empty list to store the numbers
 numbers = []
 
